623c6bf880b6e2e381be59ecfcf44855ccc267dc
belaletech <belalahmadswnbhr@gmail.com>
2023-02-06 23:30:27 +0530

    Graphics shape program

---
+++ shape.py
@@ -0,0 +1,15 @@
+import  turtle
+import  time
+def myfun():
+    colors=["red","blue","green","yellow","orange","brown"]
+    t=turtle
+    t.pensize(5)
+    t.bgcolor('black')
+    t.speed(1000)
+    for x in range(360):
+        t.pencolor(colors[x%len(colors)])
+        t.pensize(x/50)
+        t.forward(x)
+        t.left(59)
+myfun()
+time.sleep(5)
