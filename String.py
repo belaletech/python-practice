1e54353c19cf1e5b1cf70eceb7406ffd39006ab1
belaletech <belalahmadswnbhr@gmail.com>
2023-02-11 13:32:01 +0530

    String function

---
+++ String.py
@@ -0,0 +1,23 @@
+a='Belal Ahmad'
+p="this side galgotias university " \
+  "you can persue diffrent course from here!"
+print(p)
+
+# To print String at the given length or slicing the strings
+t=len(p)
+print(t)
+
+# To add two String the used + operator
+z=a+p
+
+# To add two string you with space
+print(a+" "+p)
+print("last character of p=",p[-1])
+
+# slice [Double colon is used for the print ]
+print(p[0:20])
+m="Welcome to Greater Noida"
+# [initialize : Range : increament ]
+print(m[0:15:2])
+print(m[-1:-15:-3])
+print(a[-1::-1])
---
+++ new.py
@@ -0,0 +1,13 @@
+#a=int(input("Enter your first value:\n"))
+#b=int(input("Enter your second value:"))
+#print(a+b)
+
+#p=float(input("Enter your first floting value\n"))
+#q=float(input("Enter your second floating value\n"))
+#print(p+q)
+
+x=eval(input("Enter your 1st value\n"))
+y=eval(input("Enter your 2nd value\n"))
+z=x+y
+
+print(z)
